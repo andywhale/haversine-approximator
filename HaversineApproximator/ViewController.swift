51ba5c010d3af4529bc29b1e317a0e93987cc185
Andy Whale <andy@awhale.co.uk>
2016-06-28 21:41:22 +0100

    Initial build of haversine approximate (as the crow flies)

--- HaversineApproximator/ViewController.swift
+++ HaversineApproximator/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var longitudeTextField : UITextField!
+    @IBOutlet var latitudeTextField : UITextField!
+    @IBOutlet var resultsTextView : UITextView!
+    
+    // Default is currently set to York
+    let haversineApproximator = HaversineApproximatorModel(latitude: 53.959965, longitude: -1.087298)
+    
+    @IBAction func calculateTapped(sender : AnyObject) {
+        haversineApproximator.longitude = Double((longitudeTextField.text! as NSString).doubleValue)
+        haversineApproximator.latitude = Double((latitudeTextField.text! as NSString).doubleValue)
+        
+        let distances = haversineApproximator.calculateDistance()
+        
+        var results = ""
+        for (placeName, distance) in distances {
+            results += "\(placeName): \(distance) miles\n"
+        }
+        resultsTextView.text = results
+    }
+    
+    @IBAction func viewTapped(sender : AnyObject) {
+        longitudeTextField.resignFirstResponder()
+    }
+    
+    func refreshUI() {
+        longitudeTextField.text = String(format: "%0.6f", haversineApproximator.longitude)
+        latitudeTextField.text = String(format: "%0.6f", haversineApproximator.latitude)
+        resultsTextView.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ HaversineApproximatorModel.swift
@@ -0,0 +1,104 @@
+//
+//  HaversineCalculatorModel.swift
+//  HaversineCalculator
+//
+//  Created by Andy on 21/06/2016.
+//  Copyright © 2016 andywhale. All rights reserved.
+//
+
+import Foundation
+
+class location {
+    var name: String
+    var longitude: Double
+    var latitude: Double
+    var longitudeSinHalf: Double?
+    var latitudeSinHalf: Double?
+    
+    init(name: String, longitude: Double, latitude: Double) {
+        self.name = name
+        self.longitude = longitude
+        self.latitude = latitude
+    }
+    
+    /**
+     * These functions are added to our location for performance reasons to avoid duplication
+     */
+    func getLongitudeSinHalf() -> Double {
+        if (self.longitudeSinHalf == nil) {
+            self.longitudeSinHalf = sin(self.longitude / 2)
+        }
+        return self.longitudeSinHalf!
+    }
+    
+    func getLatitudeSinHalf() -> Double {
+        if (self.latitudeSinHalf == nil) {
+            self.latitudeSinHalf = sin(self.latitude / 2)
+        }
+        return self.latitudeSinHalf!
+    }
+}
+
+class HaversineApproximator {
+    let kmToMiles: Double = 0.621371192
+    
+    func sphericalDistanceInMiles(locationFrom: location, locationTo: location) -> Double {
+        return sphericalDistanceInKm(locationFrom, locationTo: locationTo) * self.kmToMiles
+    }
+    
+    func sphericalDistanceInKm(locationFrom: location, locationTo: location) -> Double {
+        let haversineFormulae = HaversineFormulae()
+        return haversineFormulae.calculateSphericalDistance(locationFrom, locationTo: locationTo)
+    }
+}
+
+class HaversineFormulae {
+    let earthRadius: Double = 6371
+    
+    func calculateSphericalDistance(locationFrom: location, locationTo: location) -> Double {
+        let locationDelta = calculateDeltaLocation(locationFrom, locationTo: locationTo)
+        
+        let a = locationDelta.getLatitudeSinHalf() * locationDelta.getLatitudeSinHalf() +
+            cos(locationFrom.latitude * (M_PI / 180)) * cos(locationTo.latitude * (M_PI / 180)) *
+            locationDelta.getLongitudeSinHalf() * locationDelta.getLongitudeSinHalf()
+        let c = 2 * atan2(sqrt(a), sqrt(1-a))
+        
+        return self.earthRadius * c
+    }
+    
+    func calculateDeltaLocation(locationFrom: location, locationTo: location) -> location {
+        let deltaLatitude = (locationTo.latitude - locationFrom.latitude) * (M_PI / 180)
+        let deltaLongitude = (locationTo.longitude - locationFrom.longitude) * (M_PI / 180)
+        
+        return location(name: "Delta", longitude: deltaLongitude, latitude: deltaLatitude)
+    }
+}
+
+class HaversineApproximatorModel {
+    var latitude: Double
+    var longitude: Double
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    func calculateDistance() -> [String: Double] {
+        let locationFrom = location(name: "Your Location", longitude: self.longitude, latitude: self.latitude)
+        
+        var distances = [String: Double]()
+        
+        let locationA = location(name: "Bristol", longitude: 2.5833, latitude: 51.4500)
+        let locationB = location(name: "Anglesey", longitude: -4.310578, latitude: 53.255694)
+        
+        let distanceCalculator = HaversineApproximator()
+        distances[locationA.name] = round(distanceCalculator.sphericalDistanceInMiles(locationFrom, locationTo: locationA) * 100) / 100
+        distances[locationB.name] = round(distanceCalculator.sphericalDistanceInMiles(locationFrom, locationTo: locationB) * 100) / 100
+        
+        return distances
+    }
+}
+
+//let approximator = HaversineApproximatorModel(latitude: 53.9583, longitude: 1.0803)
+//approximator.calculateDistance()
+
